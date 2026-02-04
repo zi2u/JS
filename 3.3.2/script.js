dd8b063cc97950956008b31bb76d3cf9f29746a6
zi2u <zitus.cs@gmail.com>
2026-02-04 11:33:32 +0100

    nůubbuv bi

---
+++ 3.3.2/script.js
@@ -0,0 +1,21 @@
+let username = "tomáš";
+console.log(username)
+
+username = "honzík";
+console.log(username)
+
+console.log(7+4);
+console.log("7"+4);
+console.log("A"+"b");
+console.log("A"+"4");
+
+let age = 31;
+console.log(age > 20);
+
+let text = "text"
+console.log(typeof text)
+
+let a;
+console.log(a);
+let b = null;
+console.log(b)
