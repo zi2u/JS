4434830d1bbbe5df845f9b70c19f6724dd52371d
zi2u <zitus.cs@gmail.com>
2026-02-11 11:52:38 +0100

    vc gcmh

---
+++ cvičení 3.3.2/script.js
@@ -0,0 +1,15 @@
+let username = "zita";
+console.log(username)
+
+username = "majda";
+console.log(username)
+
+let age = 31;
+console.log(age)
+let age2 = 3;
+console.log(age2)
+
+let součet = age + age2;
+console.log(součet)
+
+let text = "zita majda" + 
