d90f80c099cd0494993022b58a76c23bf0eabba5
zi2u <zitus.cs@gmail.com>
2026-02-11 11:29:00 +0100

    tdjtrshtrj

--- 3.3.2/script.js
+++ 3.3.2/script.js
@@ -18,4 +18,39 @@ console.log(typeof text)
 let a;
 console.log(a);
 let b = null;
-console.log(b)
+console.log(b);
+
+let isLogggedIn = false;
+console.log(typeof isLogggedIn)
+
+// arittmetické operace
+
+let cisloA= 10;
+let cisloB = 5;
+console.log( cisloA + cisloB);
+console.log( cisloA - cisloB);
+console.log( cisloA * cisloB);
+console.log( cisloA / cisloB);
+
+// umocňování
+
+console.log(cisloA ** cisloB);
+
+// zbytek
+
+console.log(cisloA % cisloB);
+
+console.log(cisloA + cisloB * cisloB);
+console.log((cisloA + cisloB) * cisloB)
+
+// aktualizace proměnné (přiřazovací operátory)
+let number = 18;
+number = number + 1;
+number += 1;
+number -= 2;
+number *= 4;
+number /= 6;
+number %= 3;
+number++;
+number--;
+console.log(number)
