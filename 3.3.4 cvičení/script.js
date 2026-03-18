c598449ca0f82824ec28820cdbbffc36c714e9f9
zi2u <zitus.cs@gmail.com>
2026-03-18 11:42:50 +0100

    tyh

---
+++ 3.3.4 cvičení/script.js
@@ -0,0 +1,9 @@
+let  age = 20
+if (age <= 3) {
+    console.log("vstup zdarma")
+    if (age > 3 && age < 10) {
+        console.log("dostaneš 30 % slevu")
+    } if (age >= 10){
+        console.log("platíš plnou cenu")
+    }
+}
