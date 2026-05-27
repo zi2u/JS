f187bb6c5e142b69a6a8d1fae315a8bd7137afd7
zi2u <zitus.cs@gmail.com>
2026-05-27 11:32:46 +0200

    hqhřčwzh

---
+++ 3.3.10/script.js
@@ -0,0 +1,10 @@
+let myString = "hello world!"
+let myBadString = "               hellow world!"
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(1));
+console.log(myString.indexOf("l"));
+console.log(myString.replace("h","p"))
+console.log(myString.replaceAll("hello world!","palworld"))
+console.log(myBadString.trim());
---
+++ 3.3.9/script.js
@@ -0,0 +1,22 @@
+console.log("It's a nice day")
+console.log('Moje jméno je "Zita"')
+ 
+let username = "Zita"
+let message = `Moje jméno je ${username}`;
+console.log(message);
+ 
+let a = 5;
+let b = 10;
+console.log(`Součet je ${a + b}`);
+ 
+function pozdrav(name) {
+    return`Hello, ${name}!`;
+}
+ 
+console.log(`${pozdrav('Zita')}`);
+ 
+let popis = `Toto je delší text,
+který se rozprostírá
+přes několik řádků.`
+ 
+console.log(popis)
